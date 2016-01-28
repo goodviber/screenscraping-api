9ac82059b800fa1c7f61cd88d44e7e386be679d9
maxmills <8balldigitalsolutions@gmail.com>
2016-01-28 13:28:07 +0000

    model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :models
   resources :organizations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
